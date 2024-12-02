7d5d2e533d0d97393bcf5ebbc9312066d03f5355
VIDUSHISAINI29 <vidushisaini29@gmail.com>
2024-12-02 20:45:43 +0530

    first full stack dashboard pratice frontend

---
+++ src/router/index.js
@@ -0,0 +1,21 @@
+import { createRouter, createWebHistory } from "vue-router";
+
+const router = createRouter({
+   history: createWebHistory(import.meta.env.BASE_URL),
+   routes: [
+      {
+         path: "/",
+         component: () => import("@/layout/AppLayout.vue"),
+         children: [
+            {
+               path: "home",
+               name: "home",
+               component: () => import("@/pages/Home/Home.vue"),
+             
+            },
+         ],
+      },
+   ],
+});
+
+export default router;
